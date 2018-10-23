32de5e4f75025ab9c6e477692c48f5fdd3ae8da3
Brylla <bryndis14@ru.is>
2018-10-23 21:25:39 +0000

    Webpack config updated

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack')
+
+let pathsToClean = [
+  'dist'
+]
+
+module.exports = {
+	//mode: 'development',
+	entry: { main: './src/logic/index.js'},
+	output: {
+		filename: 'main.js', 
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+	new HtmlWebpackPlugin({inject: 'body'}),
+	new CleanWebpackPlugin(pathsToClean),
+	new webpack.ProvidePlugin({ 
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+	],
+
+	devServer: { 
+		port: 3000,
+		open: true,
+		proxy: {
+		//"/api": "http://localhost:8080"
+		}
+	}
+}
+
