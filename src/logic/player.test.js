d989422db02628902061532cf28af49efefda65d
Brylla <bryndis14@ru.is>
2018-10-27 23:58:39 +0000

    Implemented test for creating multiple players

--- src/logic/player.test.js
+++ src/logic/player.test.js
@@ -10,3 +10,8 @@ test('Should create a player marked X', () => {
     var testPlayer = new Player('X');
     expect(testPlayer).toEqual({symbol : 'X'});
 });
+// Testing creating multiple players 
+test('Should create a player marked X', () => {
+    var testPlayers = [new Player('X'), new Player('O')];
+    expect(testPlayers).toContainEqual({symbol : 'X'}, {symbol : 'O'});
+});
