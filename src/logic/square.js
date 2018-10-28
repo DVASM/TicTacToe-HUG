8a35cd4b8867a73387beaa6dcb5d8ea2c51c206e
Brylla <bryndis14@ru.is>
2018-10-28 22:03:30 +0000

    Refactor square class + new test for constructor

--- src/logic/square.js
+++ src/logic/square.js
@@ -1,22 +1,7 @@
-
+// Square class
 class Square {
-    constructor() {
-        this.box = '';
-    }
-    makeMove(counter) {
-        console.log('Placing symbol....');
-        if (counter === 0 && this.box === '') {
-            this.box = 'X';
-            return true;
-        }
-        else if (counter === 1 && this.box === '') {
-            this.box = 'O';
-            return true;
-        }
-        else {
-            console.log('This square has already been marked!');
-            return false;
-        }
+    constructor(box) {
+        this.box = box;
     }
 }
 
