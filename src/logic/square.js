48741d170601ae0417bcd8231e8ec36f5aea501d
Viktor Karason <vingik@gmail.com>
2018-10-28 00:42:13 +0000

    player X now shows up when makemove is used

--- src/logic/square.js
+++ src/logic/square.js
@@ -3,6 +3,13 @@ class Square {
     constructor() {
         this.box = '';
     }
-}
 
+    makeMove(counter) {
+        console.log('Placing symbol....');
+        if (counter === 0 && this.box === '') {
+            this.box = 'X';
+            return true; 
+        }
+    }
+}
 module.exports = Square; 
--- src/logic/square.test.js
+++ src/logic/square.test.js
@@ -5,3 +5,10 @@ test('Should return an empty object', () => {
     var testSquare = new Square(); 
     expect(testSquare).toEqual({box : ""});
 });
+
+test("should place a symbol to equal player X",() =>{
+    var testSquare = new Square();
+    var testCounter = 0;
+    testSquare.makeMove(testCounter);
+    expect(testSquare.box).toEqual('X');
+});
