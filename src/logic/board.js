994e2060483f5bb943ed951fa33d838604aa391d
Danielm <danieltm17@ru.is>
2018-10-28 23:17:25 +0000

    implemented makeMove function with makeMove counter unit test

--- src/logic/board.js
+++ src/logic/board.js
@@ -1,12 +1,28 @@
 // board.js
 const Square = require('./square');
+const Game = require('./game');
 
 class Board {
-    constructor(grid) {
-        this.grid = [[new Square(), new Square(), new Square()],
-        [new Square(), new Square(), new Square()],
-        [new Square(), new Square(), new Square()]
+    constructor() {
+        this.counter = 0;
+        this.grid = [[new Square(''), new Square(''), new Square('')],
+        [new Square(''), new Square(''), new Square('')],
+        [new Square(''), new Square(''), new Square('')]
         ];
     }
+
+// input player value into grid
+    makeMove(row, col, playerTurn) {
+        console.log(row, col, playerTurn + ": Made move");
+        if (this.grid[row][col].isEmpty()) {
+            this.grid[row][col].placeInput(playerTurn);
+            this.counter++;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
 }
 module.exports = Board;
--- src/logic/board.test.js
+++ src/logic/board.test.js
@@ -5,3 +5,13 @@ test('return an array with an empty string', () => {
     const testBoard = new Board();
     expect.arrayContaining([[{"box": ""}, {"box": ""}, {"box": ""}], [{"box": ""}, {"box": ""}, {"box": ""}], [{"box": ""}, {"box": ""}, {"box": ""}]]);
 });
+
+// makeMove counter test
+test('Should increment counter + 1 when called', () => {
+    const testBoard = new Board(); 
+    const testRow = 0; 
+    const testCol = 0; 
+    const testPlayer = 'X';
+    testBoard.makeMove(testRow, testCol, testPlayer);
+    expect(testBoard.counter).toBe(1);
+});
