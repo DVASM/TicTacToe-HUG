caebde796f7506cde63b70b857259c02b80732ed
Brylla <bryndis14@ru.is>
2018-10-28 22:05:20 +0000

    Implemented function to place symbol in square + tests

--- src/logic/square.js
+++ src/logic/square.js
@@ -3,6 +3,10 @@ class Square {
     constructor(box) {
         this.box = box;
     }
+    // Place symbol
+    placeInput(newsign) {
+        this.box = newsign;
+    }
 }
 
 module.exports = Square;
--- src/logic/square.test.js
+++ src/logic/square.test.js
@@ -5,3 +5,17 @@ test('Should return an empty object', () => {
     var testSquare = new Square(''); 
     expect(testSquare).toEqual({box : ''});
 });
+// Test place X in square
+test("Should place a symbol to equal player X",() =>{
+    var testSquare = new Square();
+    var testPlyer = 'X';
+    testSquare.placeInput(testPlyer);
+    expect(testSquare.box).toEqual('X');
+});
+// Test place O in square
+test("Should place a symbol to equal player O",() =>{
+    var testSquare = new Square();
+    var testPlyer = 'O';
+    testSquare.placeInput(testPlyer);
+    expect(testSquare.box).toEqual('O');
+});
