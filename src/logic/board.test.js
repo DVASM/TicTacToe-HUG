ee3cc4b1ee25ff61173b13ee5b003ff8e6d6b6ed
Danielm <danieltm17@ru.is>
2018-10-28 23:55:08 +0000

    implemented checkWinner function for vertical win condition + unit test

--- src/logic/board.test.js
+++ src/logic/board.test.js
@@ -15,3 +15,31 @@ test('Should increment counter + 1 when called', () => {
     testBoard.makeMove(testRow, testCol, testPlayer);
     expect(testBoard.counter).toBe(1);
 });
+
+// makeMove return true
+test('Should return true if input has been placed', () => {
+    const testBoard = new Board(); 
+    const testRow = 0; 
+    const testCol = 0; 
+    const testPlayer = 'X';
+    expect(testBoard.makeMove(testRow, testCol, testPlayer)).toBeTruthy(); 
+});
+
+// makeMove return false
+test('Should return true if input has been placed', () => {
+    const testBoard = new Board(); 
+    const testRow = 0; 
+    const testCol = 0; 
+    const testPlayer = 'X';
+    testBoard.makeMove(testRow, testCol, testPlayer);
+    expect(testBoard.makeMove(testRow, testCol, testPlayer)).toBeFalsy(); 
+});
+
+// checkWinner vertical test 
+test('Should return true if winning conditions have been met horizontally', () => {
+    const testBoard = new Board(); 
+    testBoard.grid[0][0].box = 'X';
+    testBoard.grid[1][0].box = 'X';
+    testBoard.grid[2][0].box = 'X';
+    expect(testBoard.checkWinner()).toBeTruthy();
+});
